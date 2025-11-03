1e7e2722a7de4df81c9810a7e7f8ecec16c37ee5
AR <ar.bjm75@gmail.com>
2025-11-03 14:09:18 +0800

    Update log message for AR Playstation

--- lapse.mjs
+++ lapse.mjs
@@ -1867,6 +1867,6 @@ setTimeout(PayloadLoader("aio_patches.bin"),500);
 log("AIO Fixes Applied.!");
 //Load GoldHEN :)
 setTimeout(PayloadLoader("goldhen_2.4b18.5.bin"),500);
-log("AR Playstation");
+msgs.innerHTML = "HEN BERHASIL Ty AR Playstation";
 
 })
